81967c2b9b0948d03fef39a8e6a7d3ae8dff176b
ebjornsfelt <ebjornsfelt@gmail.com>
2023-11-18 05:41:38 -0800

    sentiment.py kind of works
    1000 positive reviews and 1000 negative reviews in the combined folder. The sentiment analysis module I'm using returns 1742 positive reviews, 258 negatives and 0 neutrals

--- sentiment.py
+++ sentiment.py
@@ -25,38 +25,13 @@ def review_analyzer(reviewpath):
     return positive_rev, negative_rev, neutral_rev
 
 
-pos_folder_path = 'D:/SFU WORK/Fall 2023/SDA 250/Assignment2/pos'
-neg_folder_path = 'D:/SFU WORK/Fall 2023/SDA 250/Assignment2/neg'
+folder_path = 'D:/SFU WORK/Fall 2023/SDA 250/Assignment_2/combined'
 
-positive_reviews, negative_reviews, neutral_reviews = review_analyzer(pos_folder_path)
 
-# Print the results
+positive_reviews, negative_reviews, neutral_reviews = review_analyzer(folder_path)
+
+
 print(f"Number of positive reviews: {len(positive_reviews)}")
 print(f"Number of negative reviews: {len(negative_reviews)}")
 print(f"Number of neutral reviews: {len(neutral_reviews)}")
 
-# print("Negative folder analysis:")
-# for filename in os.listdir(neg_folder_path):
-#     print(filename)
-                
-                
-#                 if blob.sentiment.polarity > 0:
-#                     positive_rev.append(review_text)
-#                 elif blob.sentiment.polarity < 0:
-#                     negative_rev.append(review_text)
-
-#     return positive_rev, negative_rev
-
-# # Specify the paths to the positive and negative review folders
-# pos_folder_path = r'D:\SFU WORK\Fall 2023\SDA 250\Assignment2\pos'
-# neg_folder_path = r'D:\SFU WORK\Fall 2023\SDA 250\Assignment2\neg'
-
-# # Analyze positive reviews
-# positive_reviews, negative_reviews = review_analyzer(pos_folder_path)
-
-# # Analyze negative reviews
-# positive_reviews, negative_reviews = review_analyzer(neg_folder_path)
-
-# # Print the results
-# print(f"Number of positive reviews: {len(positive_reviews)}")
-# print(f"Number of negative reviews: {len(negative_reviews)}")
